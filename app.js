b2e17920c38158dba3c249c98c54707cf1f16a0c
matias-rivera <juanmatiasrivera@outlook.com>
2022-02-15 17:54:00 -0300

    project init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/database");
+
+// Setting up config.env file variables
+dotenv.config({ path: "./config/config.env" });
+
+//Connecting to database
+connectDB();
+
+// Importing routes
+const jobs = require("./routes/jobs");
+
+app.use("/api/v1", jobs);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT} in ${process.env.NODE_ENV} mode`);
+});
---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => {
+      console.log(
+        `MongoDB Database connected with host: ${conn.connection.host}`
+      );
+    });
+};
+
+module.exports = connectDB;
